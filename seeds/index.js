3f2e15b9833fdfeecb128db679cd3f5dfe023869
Abhishek Anand <anandabhishek3607@gmail.com>
2023-07-19 00:45:12 +0530

    image upload issue resolved

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect("mongodb+srv://sharpviking:l9a53607@cluster0.0maezhz.mongodb.net/test", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
