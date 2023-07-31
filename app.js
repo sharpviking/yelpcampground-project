b89e24d80f4f31063cc47e0f1cd01c97c0824d60
Abhishek Anand <anandabhishek3607@gmail.com>
2023-07-31 13:56:16 +0530

    error handling

--- app.js
+++ app.js
@@ -38,10 +38,16 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+
+
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (e) {
+        next(e);
+    }
 })
 
 app.get('/campgrounds/:id', async (req, res,) => {
@@ -66,7 +72,9 @@ app.delete('/campgrounds/:id', async (req, res) => {
     res.redirect('/campgrounds');
 })
 
-
+app.use((err, req, res, next) => {
+    res.send('Oh boy, something went wrong!')
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
