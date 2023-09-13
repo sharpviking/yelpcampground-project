ce3e4b01bb68da1ea24759b31f2c4a451e968927
Abhishek Anand <anandabhishek3607@gmail.com>
2023-09-13 18:06:06 +0530

    adding stars as reviews

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,29 +2,23 @@ const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
-
 const { isLoggedIn } = require('../middleware');
 const { validateCampground, isAuthor } = require('../middleware')
-
-
 const Campground = require('../models/campground');
-const campground = require('../models/campground');
 
 
-
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isAuthor, isLoggedIn, catchAsync(campgrounds.deleteCampground));
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isAuthor, isLoggedIn, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,13 @@ const User = require('../models/user');
 const users = require('../controllers/users')
 const { storeReturnTo } = require('../middleware');
 
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', users.renderRegister);
-
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
-
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout);
 
