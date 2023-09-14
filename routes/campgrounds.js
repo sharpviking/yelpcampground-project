da7d45d12e107b12c712b3c692323e7ef57e5da4
Abhishek Anand <anandabhishek3607@gmail.com>
2023-09-14 13:52:57 +0530

    using multer  to upload real images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,11 +5,18 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn } = require('../middleware');
 const { validateCampground, isAuthor } = require('../middleware')
 const Campground = require('../models/campground');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('It worked');
+
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
