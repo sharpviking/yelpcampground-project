997fada36c468afee600c54dccedb508f41a6685
Abhishek Anand <anandabhishek3607@gmail.com>
2023-09-23 15:55:50 +0530

    displaying a map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,13 +6,7 @@ const map = new mapboxgl.Map({
     zoom: 4, // starting zoom
 });
 
-// new mapboxgl.Marker()
-//     .setLngLat(campground.geometry.coordinates)
-//     .setPopup(
-//         new mapboxgl.Popup({ offset: 25 })
-//             .setHTML(
-//                 `<h3>${campground.title}</h3><p>${campground.location}</p>`
-//             )
-//     )
-//     .addTo(map)
+new mapboxgl.Marker()
+    .setLngLat([-74.5, 40])
+    .addTo(map);
 
