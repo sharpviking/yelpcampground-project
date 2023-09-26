1ef876504e30f3249b9c8be3304659362fd8a31f
Abhishek Anand <anandabhishek3607@gmail.com>
2023-09-26 15:09:58 +0530

    changing cluster size and colour

--- seeds/index.js
+++ seeds/index.js
@@ -21,10 +21,11 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            // Your User Id
             author: '64fec8035f3e6c281cfae490',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
