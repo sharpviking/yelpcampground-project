7b9b986cf0c44e65c91cd65dac443a48e9b4cafa
Abhishek Anand <anandabhishek3607@gmail.com>
2023-09-26 17:22:26 +0530

    adding cluster popup with properties

--- models/campground.js
+++ models/campground.js
@@ -12,6 +12,9 @@ const ImageSchema = new Schema({
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('upload', '/upload/w_250');
 })
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -38,8 +41,19 @@ const CampgroundSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
-    ]
-});
+    ],
+
+}, opts);
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`;
+})
+
+
+
+
+
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
         await Review.deleteMany({
